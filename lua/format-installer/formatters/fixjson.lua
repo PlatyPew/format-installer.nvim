4bde1550231ddc4a60f20818823f881d3d54c4ae
Platy <daryl.limkz@gmail.com>
2022-01-10 13:12:33 +0800

    feat: added fixjson
    Signed-off-by: Platy <daryl.limkz@gmail.com>

---
+++ lua/format-installer/formatters/fixjson.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+function M.install(path, formatter)
+    M.dependencies = { "npm" }
+    if vim.fn.executable(M.dependencies[1]) == 1 then
+        vim.fn.system({ M.dependencies[1], "install", "--prefix", path, "fixjson" })
+        return true
+    else
+        print("Failed to install " .. formatter .. "! Missing dependencies: " .. M.dependencies[1])
+        return false
+    end
+end
+
+function M.get_path(path)
+    return path .. "/node_modules/.bin/fixjson"
+end
+
+return M
